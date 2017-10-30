9a470701785ffd5d010fffe45e4f6534fceb9205
xiaolin <462441355@qq.com>
2017-10-30 20:47:59 +0800

    * 打包准备

---
+++ app-web/app/Http/Controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Huifang\Web\Http\Controllers;
+
+
+class HomeController extends BaseController
+{
+    public function index()
+    {
+        dump(1);
+    }
+
+}
--- app-web/routes/web.php
+++ app-web/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group([], function($router){
+    $router->any('home/index', 'HomeController@index')->name('home.index');
+});
