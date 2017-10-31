a6d9a6d090dc5769d5e51577bb741d1c0005e0d4
xiaolin <462441355@qq.com>
2017-10-31 09:55:22 +0800

    * r.js打包移植

--- app-web/app/Http/Controllers/HomeController.php
+++ app-web/app/Http/Controllers/HomeController.php
@@ -6,6 +6,9 @@ class HomeController extends BaseController
 {
     public function index()
     {
+        $this->title = '首页';
+        $this->file_css = 'css/index/index';
+        $this->file_js = 'pages/index/index';
         return $this->view('home.index');
     }
 
