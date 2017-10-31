7c733c999f1cba40ee98b6d03a8b9b9ff4d49dc9
xiaolin <462441355@qq.com>
2017-10-31 12:45:51 +0800

    * 增加admin入口

--- app-admin/config/services.php
+++ app-admin/config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model' => Huifang\User::class,
+        'model' => Huifang\Admin\User::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
