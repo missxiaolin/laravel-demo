08646afa485ac36805f0a9bc2284e66965a1a017
xiaolin <462441355@qq.com>
2017-12-05 16:51:25 +0800

    * 增加helpers

---
+++ core/Support/helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!function_exists('api_response')) {
+
+    function api_response($data, $code = '0', $msg = 'ok')
+    {
+        $json = [
+            'data' => $data,
+            'code' => $code,
+            'msg' => $msg,
+            'time' => (string)time(),
+            '_ut' => (string)round(microtime(TRUE) - $_SERVER['REQUEST_TIME_FLOAT'], 5),
+        ];
+
+        return response()->json($json);
+    }
+}
