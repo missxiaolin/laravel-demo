bfc01d3ee48fef00dda9f1c85f997d0a65519d9b
xiaolin <462441355@qq.com>
2017-12-28 17:02:36 +0800

    * 增加配置

--- app-crm/config/filesystems.php
+++ app-crm/config/filesystems.php
@@ -50,8 +50,15 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path('uploads'),
+            'url' => env('APP_URL').'/uploads',
+            'visibility' => 'public',
+        ],
+
+        'admin' => [
+            'driver' => 'local',
+            'root' => public_path('uploads'),
+            'url' => env('APP_URL').'/uploads',
             'visibility' => 'public',
         ],
 
